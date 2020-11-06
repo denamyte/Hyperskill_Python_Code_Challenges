5a5c801196a0caead87cf5afd5f1b192125b1c7f
denamyte <denamyte@gmail.com>
2020-11-06 02:51:08 +0200

    2020-11-06: 4 problems

---
+++ Problems/Computer hours/main.py
@@ -0,0 +1,7 @@
+hours = float(input())
+if hours < 2:
+    print("That seems reasonable")
+elif hours < 4:
+    print("Do you have time for anything else?")
+else:
+    print("Don't forget to take breaks!")
---
+++ Problems/Grade/main.py
@@ -0,0 +1,11 @@
+percent = float(input()) / float(input())
+if percent < .6:
+    print('F')
+elif percent < .7:
+    print('D')
+elif percent < .8:
+    print('C')
+elif percent < .9:
+    print('B')
+else:
+    print('A')
