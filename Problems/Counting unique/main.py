012108b13fbf7a3bc2a1e0bb6fa73c7c472586c8
denamyte <denamyte@gmail.com>
2020-11-20 01:45:19 +0200

    2020-11-20: 4 problems

---
+++ Problems/Counting unique/main.py
@@ -0,0 +1,11 @@
+# The following code is needed for us to check your answer, do not modify it, please.
+students = json.loads(input())
+Belov = students['Belov']
+Smith = students['Smith']
+Sarada = students['Sarada']
+
+# Your code here. Work with the variables 'Belov', 'Smith', and 'Sarada'
+subjects = set()
+for student in [Belov, Smith, Sarada]:
+    subjects.update(student)
+print(len(subjects))
