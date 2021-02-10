d78996547343432cb4a8afcc848b6356dca3f9e9
denamyte <denamyte@gmail.com>
2021-02-11 01:17:56 +0200

    2021-02-11: 3 problems

---
+++ Problems/Let's rock/main.py
@@ -0,0 +1,8 @@
+class RockBand:
+    genre = "rock"
+    key_instruments = ["electric guitar", "drums"]
+    n_members = 4
+
+
+metallica = RockBand()
+print(metallica.genre, metallica.n_members, metallica.key_instruments, sep='\n')
