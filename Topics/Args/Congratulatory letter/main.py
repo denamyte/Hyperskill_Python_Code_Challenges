6d45a738478a1331dd7375c834aec6616843807f
denamyte <denamyte@gmail.com>
2021-04-15 20:05:13 +0300

    2021-04-53: 1 problem

--- Topics/Args/Congratulatory letter/main.py
+++ Topics/Args/Congratulatory letter/main.py
@@ -1,2 +1,4 @@
-def congratulations():
-    pass
+def congratulations(pm, tester, *devs):
+    print('Happy New Year! Take care of yourself and your loved ones!\nFor:')
+    for name in [pm, tester, *devs]:
+        print(name)
