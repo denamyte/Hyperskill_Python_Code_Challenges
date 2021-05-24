63e64fbce5807821ead76a18e46c792fb056693c
denamyte <denamyte@gmail.com>
2021-05-25 01:19:36 +0300

    2021-05-24: 1 problem

--- Topics/Dictionary methods/Big family/main.py
+++ Topics/Dictionary methods/Big family/main.py
@@ -2,4 +2,7 @@
 first_family = json.loads(input())
 second_family = json.loads(input())
 
-# Work with the 'first_family' and 'second_family' and create a new dictionary
+family = {}
+family.update(first_family)
+family.update(second_family)
+print(family)
