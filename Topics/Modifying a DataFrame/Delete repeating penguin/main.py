731cb847de2f73fcecd5b112db20d17d7a904c04
denys <denamyte@gmail.com>
2023-06-25 00:04:42 +0300

    2023-06-24: 2 problems

---
+++ Topics/Modifying a DataFrame/Delete repeating penguin/main.py
@@ -0,0 +1,3 @@
+# your code here. The dataset is already loaded, the variable that contains the DataFrame is called penguins_example
+penguins_example.drop_duplicates(inplace=True)
+print(penguins_example)
--- Topics/Modifying a DataFrame/Growing square/main.py
+++ Topics/Modifying a DataFrame/Growing square/main.py
@@ -3,5 +3,5 @@ import pandas as pd
 df = pd.DataFrame({'a': [1, 4], 'b': [2, 5]})
 
 df['c'] = [3, 6]
-df2 = pd.DataFrame([{'a': 7, 'b': 8, 'c': 9}])
+df2 = pd.DataFrame({'a': [7], 'b': [8], 'c': [9]})
 df = pd.concat([df, df2], ignore_index=True)
